d5bdc3f99005efe181dfea11a181237871c1f8ce
vademyanchik <vadim.demyanchik@gmail.com>
2017-07-23 23:31:03 +0300

    Add main page.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,87 @@
 <template>
-  <div id="app">
+<div class="app">
+  <div class="header">
+    <h1>Alis web vue</h1>
   </div>
+    <div class="content"> 
+      <div class="block">
+        <div class="demonstration">Показать последние книги за выбранный период</div>
+        <el-date-picker class="vadim"
+          lang="ru"
+          v-model="value"
+          type="datetimerange"
+          :picker-options="pickerOptions"
+          placeholder="Выбрать период"
+          align="center">
+        </el-date-picker>
+    </div>
+    
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      pickerOptions: {
+        shortcuts: [{
+          text: 'Неделю',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - (3600 * 1000 * 24 * 7));
+            picker.$emit('pick', [start, end]);
+          },
+        }, {
+          text: 'Месяц',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - (3600 * 1000 * 24 * 30));
+            picker.$emit('pick', [start, end]);
+          },
+        }, {
+          text: 'Три месяца',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - (3600 * 1000 * 24 * 90));
+            picker.$emit('pick', [start, end]);
+          },
+        }],
+      },
+      value: '',
+    };
+  },
 };
 </script>
 
-<style>
+<style >
+
+.app {
+  background: lightgreen;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow: auto;
+}
+
+.header {
+  height: 30%;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+}
+
+h1 {
+  text-align: center;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import ElementUI from 'element-ui';
+import lang from 'element-ui/lib/locale/lang/ru-RU';
+import locale from 'element-ui/lib/locale';
+import '../theme/index.css';
+
+locale.use(lang);
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
